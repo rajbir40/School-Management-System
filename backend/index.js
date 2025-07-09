420f7e8f4a222d1fe94458857e2b371a9748c3c5
rajbir <158246329+rajbir40@users.noreply.github.com>
2025-07-09 14:52:15 +0530

    C:/Program Files/Git/get

--- backend/index.js
+++ backend/index.js
@@ -26,6 +26,10 @@ mongoose
 
 app.use('/', Routes);
 
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
 app.listen(PORT, () => {
     console.log(`Server started at port no. ${PORT}`)
 })
